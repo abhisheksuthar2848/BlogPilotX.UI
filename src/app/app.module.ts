d94788047b9d6b1f0d86c89106c6e4356450c1fd
Abhishek Suthar <abhisheksuthar2848@gmail.com>
2023-10-24 23:25:21 +0530

    Initail commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NevbarComponent } from './Core/Component/nevbar/nevbar.component';
+import { CategoryListComponent } from './Features/Category/category-list/category-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NevbarComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,
