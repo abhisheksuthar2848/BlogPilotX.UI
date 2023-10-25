a0279362c623377f143b33167395522a3b0a1044
Abhishek Suthar <abhisheksuthar2848@gmail.com>
2023-10-26 00:15:15 +0530

    data retrieval subscribe method to async pipe

--- src/app/Features/Category/category-list/category-list.component.ts
+++ src/app/Features/Category/category-list/category-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../category.service';
 import { Category } from '../Models/Category.model';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -10,24 +11,27 @@ import { Category } from '../Models/Category.model';
 })
 export class CategoryListComponent implements OnInit {
 
-categories ?: Category[];
+// categories ?: Category[];
+categories$?:Observable<Category[]>;
+
 
   constructor(private categoryService: CategoryService){
 
   }
 
   ngOnInit(): void {
-    this.categoryService.getAllCategories().subscribe({
-       next:(Response)=> {
-         console.log(Response);
-         this.categories=Response;
-      },
-       error:(err)=>{
-        console.log(err);
-       }
+    // this.categoryService.getAllCategories().subscribe({
+    //    next:(Response)=> {
+    //      console.log(Response);
+    //      this.categories=Response;
+    //   },
+    //    error:(err)=>{
+    //     console.log(err);
+    //    }
 
+  // });
+   this.categories$ = this.categoryService.getAllCategories();
 
-    });
   }
 
 }
